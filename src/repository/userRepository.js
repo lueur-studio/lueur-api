731b655b702ed5745a955c95e393147152d351eb
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2025-11-19 12:52:02 -0500

    feat: add bcrypt for password hashing in user management

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 const userRepository = {
   findAll: async () => {
@@ -14,12 +15,18 @@ const userRepository = {
   },
 
   create: async (userData) => {
+    if (userData.password) {
+      userData.password = await bcrypt.hash(userData.password, 10);
+    }
     return await User.create(userData);
   },
 
   update: async (id, updateData) => {
     const user = await User.findByPk(id);
     if (!user) return null;
+    if (updateData.password) {
+      updateData.password = await bcrypt.hash(updateData.password, 10);
+    }
     return await user.update(updateData);
   },
 
