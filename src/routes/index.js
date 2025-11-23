aa34be0c839b0801e00744b9ad280ea26087df89
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2025-11-23 13:37:51 -0500

    feat: add event validation middleware and routes for event management

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
+const eventRoutes = require("./eventRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
