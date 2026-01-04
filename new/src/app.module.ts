96e3e30a45aa8f9ddd7f80a99502bccc99349cbf
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2026-01-03 21:41:33 -0500

    fix: configure global settings

--- new/src/app.module.ts
+++ new/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import awsConfig from './config/aws.config';
+import encryptionConfig from './config/encryption.config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, jwtConfig, awsConfig, encryptionConfig],
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions =>
+        configService.get<TypeOrmModuleOptions>('database')!,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
